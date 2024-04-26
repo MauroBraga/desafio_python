58714234d05cb152dbf87ccee1f923155e79bb2f
mauro <mrb0305@gmail.com>
2024-04-26 10:01:30 -0300

    deletar

--- desafio.py
+++ desafio.py
@@ -16,6 +16,16 @@ def updateContato(contatos,indice,nome,telefone,email):
         print(f"Conato {nome}  não foi alterado!")
     return
 
+def marcareContato(contatos,indice):
+    indice_ajustado = int(indice) - 1
+    if indice_ajustado >= 0 and indice_ajustado < len(contatos):
+        nome = contatos[indice_ajustado]["nome"]
+        contatos[indice_ajustado]["favorito"] =  False if contatos[indice_ajustado]["favorito"] else True
+        print(f"Conato {nome}  foi alterado!")
+    else:
+        print(f"Conato não foi alterado!")
+    return
+
 def findAll(contatos):
     print("\nLista de contatos:")
     for indice, contato in enumerate(contatos, start=1):
@@ -24,6 +34,28 @@ def findAll(contatos):
         telefone = contato["telefone"]
         email = contato["email"]
         print(f"{indice}. [{status}] Nome:{nome} telefone: {telefone} e-mail: {email}")
+    return
+
+def findAllFavoritos(contatos):
+    print("\nLista de contatos favoritos:")
+    for indice, contato in enumerate(contatos, start=1):
+        if(contato["favorito"]):
+            status = "✓" if contato["favorito"] else " "
+            nome = contato["nome"]
+            telefone = contato["telefone"]
+            email = contato["email"]
+            print(f"{indice}. [{status}] Nome:{nome} telefone: {telefone} e-mail: {email}")
+    return
+
+def deleteContato(contatos,indice):
+    indice_ajustado = int(indice) - 1
+    if indice_ajustado >= 0 and indice_ajustado < len(contatos):
+        contato = contatos[indice_ajustado]
+        nome = contatos[indice_ajustado]["nome"]
+        contatos.remove(contato)
+        print(f"Conato {nome}  foi deletado!")
+    else:
+        print(f"Conato não foi deletado!")
     return
 
 contatos=[]
@@ -53,5 +85,13 @@ while True:
       telefone = input("Digite o telefone : ")
       email = input("Digite o email : ")
       updateContato(contatos,indice, nome,telefone,email)
+  elif escolha == "4":
+      indice = int(input("Digite o indice : "))
+      marcareContato(contatos,indice)
+  elif escolha == "5":
+      findAllFavoritos(contatos)
+  elif escolha == "6":
+      indice = int(input("Digite o indice : "))
+      deleteContato(contatos,indice)
   elif escolha == "7":
       break
